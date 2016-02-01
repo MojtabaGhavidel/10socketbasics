5dbde2b599b6da13331b4365b49bc7e4980e388c
Mojtaba Ghavidel <mojtabaghavidel@gmail.com>
2016-02-01 13:10:06 -0800

    Add message to UI 10_6

--- public/js/app.js
+++ public/js/app.js
@@ -10,26 +10,36 @@ socket.on('connect', function(){
 console.log('msg from FronEnd : User connected');
 });
 
-
+//tabE call back data ro migire ke dar inja 'message' object hast dar server.js sakhte shude
+//dar inja frontEnd muntazere event hast va vaghti rokh mide on ro migire va print mikune
+//har bar ke messag i miyad ejra mishe
 socket.on('message', function (message){
 	console.log('New message:');
-	console.log(message.text);
+	console.log(message.text); 
+
+	//ezafe kardane payame jadid be html
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 
 });
 
-//Handles submiting of new message
-//
+//Handles submiting of new message 
+//(in bakhsh daryaft va ersal payam ro az tarigh form anjam mide)<<<<<<<
+//dalile gozashtane alamate $ in hast ke vegim in motaghyer 1 morede JQ ro dar khodesh dare.yani be methodhaye JQ dastresi dare
 var $form = jQuery('#message-form');
 
 $form.on('submit', function (event) {
-	//
+	//baraye raveshe sabte foorm hast va az refresh safhe jelo giri mikune
 	event.preventDefault();
 
-	//
+	//ersale payam be server.noE event = message
 	socket.emit('message', {
-		text: $form.find('input[name=message]').val()
+		//.val = method
+		text: $form.find('#text-box').val()
 	});
-			///???
+			///matne dakhele text ro pak kun
 			$('#text-box').val('');
+			$('#text-box').disabled = true;
+
+
 
 });
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 //server e HTTP ma.aval mazhole http farakhni mishe.methode server farakahni mishe va appe express ro behesh midim.
-//be node migim 1 servere jadid ejra kun va az app be onvane "boilrplate" estefade kun yani harchizi ke appe express behesh gosh bede
+//be node migim 1 servere jadid ejra kun va az app be onvane "boilerplate" estefade kun yani harchizi ke appe express behesh gosh mide
 //server ham bayad behesh gosh bede
 var http = require('http').Server(app);
 
@@ -23,33 +23,34 @@ app.use(express.static(__dirname + '/public'));
 
 //server ro ejra mikune
 //function, call backi hast k bad az ejrae server return mishe
-
 //in khat ro zire app.use gharar dadim
-//ON montazere events mimone.avalin argoman esme event hast.
+//ON montazere events mimone.avalin argoman esme event hast ke dar 'socket.emit' sakhtim
+//'socket' dar inja 1 computeri hast ke vasl shude na chand computer.
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
-
+	//mikham payam ha ro vase hame befrestim
 	//message = esme event,function = callback
 	//message(narenji/daron function) object hast
 	//in bakhsh montazere event mimone va on ro be tamame connection ha emit mikne
 	//in bakhsh dar SERVER log mikune faghat
 	socket.on('message', function (message){
-		console.log('Message recieved' + message.text);
+		console.log('Message recieved :' + message.text);
 
-		//in bakhsh baraye CLIENT emit mikune
+		//in bakhsh baraye CLIENT emit mikune nabashe payam ha send neishan
 		//io.emit = baraye khode ersal kunande ham mire
-		socket.broadcast.emit('message', message)
-	})
+		//socket.broadcast.emit('message', message)
 
-
+		io.emit('message', message)
+	});
 
 	//objecte so socket va methode emit ke 1 event ro emit mikune
 	//emit 1 argoman migire 1:esme event(harchi ke mikhaim) 2:data ke mifrestim.1 argoman inja mitonim estedae kunim
 	//pas behtare OBJECT be kar bebarim
+	//in bakhsh ro dar DEV CONSOLE bezarim payam ro mifreste
 
 	socket.emit('message', {
-		text :'Welcome to the cht application!'
+		text :'Welcome to the chat application!'
 	});
 
 });
